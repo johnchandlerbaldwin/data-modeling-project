2a53989cd55bf0d3d8aa7dd0179af32053a51190
John Baldwin <johnchandlerbaldwin@gmail.com>
2023-12-14 21:41:38 +0000

    corrected stg_hdi

--- models/staging/stg_hdi.sql
+++ models/staging/stg_hdi.sql
@@ -2,14 +2,15 @@
 
 WITH hdi_stg1 AS (
 
-    SELECT RowNumber, country, MAX(HDI) OVER (PARTITION BY grp ORDER BY RowNumber ASC) AS hdi
-    FROM (
         SELECT ANation AS country,
         ARowNumber AS RowNumber,
         AHDI AS HDI,
         COUNT(AHDI) OVER (ORDER BY ARowNumber ASC) AS grp
         FROM economic_indicators.hdi
-        WHERE SAFE_CAST(AHDI AS NUMERIC) IS NOT NULL) t
-)
+        WHERE SAFE_CAST(AHDI AS NUMERIC) IS NOT NULL),
 
-SELECT country, hdi FROM hdi_stg1
+    hdi_stg2 AS (
+    SELECT RowNumber, country, MAX(HDI) OVER (PARTITION BY grp ORDER BY RowNumber ASC) AS hdi
+    FROM hdi_stg1)
+
+SELECT country, hdi FROM hdi_stg2
